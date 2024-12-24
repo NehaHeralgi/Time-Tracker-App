a8a640abb7bc9eac7040c9afac8163585f0ebe92
Neha Heralgi <nehaheralgi7506228705@gmail.com>
2024-12-24 14:23:27 +0530

    Home changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { DividerModule } from 'primeng/divider';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,CalendarModule ,FormsModule ,DividerModule ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  date: Date | null = null
+  date1: Date | null = null
+  date3: Date | null = null
+  value: string | null = null
+  value1: string | null = null
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(), 
+    importProvidersFrom(BrowserAnimationsModule)
   ],
 }).catch((err) => console.error(err));
